5b3cd501b7ebec97955bf6ef141270031456447d
Igor <igordevmail@gmail.com>
2025-03-14 16:14:48 +0100

    Add template project.

---
+++ BLEApp/BLEApp/BLEAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BLEAppApp.swift
+//  BLEApp
+//
+//  Created by Ihor Ilin on 14.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct BLEAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
